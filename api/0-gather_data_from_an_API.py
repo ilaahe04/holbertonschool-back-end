4b376b3306daeec62f0c9acdccc89a8a219d5a84
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-23 16:55:04 +0400

    Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""returns information about his/her TODO list progress."""
+
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    if len(argv) < 2:
+        exit()
+
+    user_name = requests.get(f"https://jsonplaceholder.\
+typicode.com/users/{argv[1]}").json().get('name')
+
+    todos = requests.get(f"https://jsonplaceholder.typicode.\
+com/todos?userId={argv[1]}").json()
+
+    total = len(todos)
+    completed = 0
+    titles = ""
+    for i in todos:
+        if i['completed']:
+            completed += 1
+            titles += "\t " + i.get('title') + "\n"
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, completed, total))
+    if titles != "":
+        print(titles[:-1])
