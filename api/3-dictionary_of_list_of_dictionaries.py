d86b8042d4e32e5d9f5999c1f61d9bdfb219e7d8
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-23 21:19:32 +0400

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -18,10 +18,10 @@ typicode.com/users/").json()
     for user in user_list:
         todos = requests.get(f"https://jsonplaceholder.typicode.\
 com/todos?userId={user.get('id')}").json()
-    list_n = []
-    for todo in todos:
-        list_n.append({'username': user.get('username'),
-                       'task': todo.get('title'),
-                       'completed': todo.get('completed')})
-    dict_n[user.get('id')] = list_n
+        list_n = []
+        for todo in todos:
+            list_n.append({'username': user.get('username'),
+                           'task': todo.get('title'),
+                           'completed': todo.get('completed')})
+        dict_n[user.get('id')] = list_n
     json.dump(dict_n, open(f"todo_all_employees.json", "w"))
