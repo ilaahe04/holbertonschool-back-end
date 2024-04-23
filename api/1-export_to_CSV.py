86661a9f913e8e68c698824eb10d33515b88ad5d
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-23 17:26:31 +0400

    Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""returns information about his/her TODO list progress."""
+
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+    import csv
+
+    if len(argv) < 2:
+        exit()
+
+    username = requests.get(f"https://jsonplaceholder.\
+typicode.com/users/{argv[1]}").json().get('username')
+
+    todos = requests.get(f"https://jsonplaceholder.typicode.\
+com/todos?userId={argv[1]}").json()
+
+    writer = csv.writer(open(f'{argv[1]}.csv', 'w'), quoting=csv.QUOTE_ALL)
+
+    for todo in todos:
+        writer.writerow([todo.get('userId'), username,
+                         todo.get('completed'), todo.get('title')])
