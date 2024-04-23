2e5c2f03b2cc6f796b33b01a427d9d957092a4e6
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-23 21:17:08 +0400

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -10,19 +10,18 @@ if __name__ == "__main__":
     import json
     import requests
 
-
     dict_n = {}
 
     user_list = requests.get(f"https://jsonplaceholder.\
 typicode.com/users/").json()
-    
+
     for user in user_list:
         todos = requests.get(f"https://jsonplaceholder.typicode.\
 com/todos?userId={user.get('id')}").json()
     list_n = []
     for todo in todos:
         list_n.append({'username': user.get('username'),
-                       :wq'task': todo.get('title'),
-                             'completed': todo.get('completed')})
+                       'task': todo.get('title'),
+                       'completed': todo.get('completed')})
     dict_n[user.get('id')] = list_n
     json.dump(dict_n, open(f"todo_all_employees.json", "w"))
